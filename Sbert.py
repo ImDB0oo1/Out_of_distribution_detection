792530cc59feee3b4e15f475c65b74470614a65a
unknown <danialbma77@gmail.com>
2024-04-13 20:38:46 +0330

    Adding 20newsgroup dataset

--- Sbert.py
+++ Sbert.py
@@ -3,23 +3,39 @@ import pandas as pd
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_20newsgroups
+
+
+categories = [
+    'alt.atheism',
+    'talk.religion.misc',
+    'comp.graphics',
+    'sci.space'
+]
+
+data_train = fetch_20newsgroups(
+    subset='train', 
+    categories=categories, 
+    shuffle=True, 
+    random_state=42
+)
+OOD_data = data_train.data
 
 
 IMDB_data = pd.read_csv("IMDB Dataset.csv")
 
-corpus = IMDB_data['review'][0:10].tolist()
-topics = ["football", "economy","politics", "coffee", "watching", "episode", "movie", "film", "like", "good", "bad", "arts", "think", "horror", "action", "story", "theatrer", "filmmakers", "performance", "name"]
+corpus = IMDB_data['review'][0:5].tolist()
+topics = ["watching", "episode", "movie", "film", "like", "good", "bad", "arts", "think", "horror", "action", "story", "theatrer", "filmmakers", "performance", "name"]
 
-corpus.append("This man doing some great work in comunity and help to have better country")
 model = SentenceTransformer("multi-qa-MiniLM-L6-cos-v1")
 
-
 doc_embeddings = model.encode(corpus)
+OOD_embeddings = model.encode(OOD_data[0:5])
 topic_embeddings = model.encode(topics)
-print(topic_embeddings[0].shape)
+print(type(OOD_embeddings[0]))
 
 # Set similarity threshold
-threshold = 0.2
+threshold = 0.17
 
 # Create graph
 G = nx.Graph()
@@ -30,13 +46,25 @@ for i, topic in enumerate(topics):
 
 # Add documents as nodes and connect to topics based on similarity
 for i, doc_embedding in enumerate(doc_embeddings):
-    G.add_node(f"doc{i}", type='document', bipartite=1)
+    G.add_node(f"ID_doc{i}", type='ID document', bipartite=1, label=0, embedding=np.array2string(doc_embedding, separator=',').strip('[]'))
     for j, topic_embedding in enumerate(topic_embeddings):
         similarity = util.cos_sim(topic_embedding, doc_embedding)
         #print(similarity, j)
         #print(np.linalg.norm(doc_embedding))
         if similarity > threshold:
-            G.add_edge(topics[j], f"doc{i}", weight=similarity[0][0].item())
+            G.add_edge(topics[j], f"ID_doc{i}", weight=similarity[0][0].item())
+
+# Add documents as nodes and connect to topics based on similarity
+for i, doc_embedding in enumerate(OOD_embeddings):
+    G.add_node(f"OOD_doc{i}", type='OOD document', bipartite=1, label=1, embedding=np.array2string(doc_embedding, separator=',').strip('[]'))
+    for j, topic_embedding in enumerate(topic_embeddings):
+        similarity = util.cos_sim(topic_embedding, doc_embedding)
+        #print(similarity, j)
+        #print(np.linalg.norm(doc_embedding))
+        if similarity > threshold:
+            G.add_edge(topics[j], f"OOD_doc{i}", weight=similarity[0][0].item())
+
+nx.write_graphml(G, '5000ID_2000OOD.graphml')
 
 # Plot graph
 plt.figure(figsize=(10,8))
@@ -46,6 +74,8 @@ pos = nx.bipartite_layout(G, topics)
 nx.draw(G, pos, with_labels=True, node_size=500, font_size=5, font_weight='bold')
 edge_labels = nx.get_edge_attributes(G, 'weight')
 nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size = 8)
+labels = nx.get_node_attributes(G, 'label')
+nx.draw_networkx_labels(G, pos, labels=labels)
 plt.title('Graph of topics and documents')
 plt.show()
 
